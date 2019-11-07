c6b93387c96858e6a8f54a9b4e834e8bb8526a02
sqrtthree <imsqrtthree@gmail.com>
2019-11-07 23:20:16 +0800

    style: Format code

--- src/components/TheCopyright/index.vue
+++ src/components/TheCopyright/index.vue
@@ -27,7 +27,7 @@ export default {
 }
 </script>
 
-<style lang="scss" >
+<style lang="scss">
 @import '~@/styles/app';
 
 .copyright {
--- src/views/Auth/Login.vue
+++ src/views/Auth/Login.vue
@@ -3,10 +3,19 @@
     <div class="login-form login-form__container">
       <el-form label-position="top" :model="form">
         <el-form-item label="用户名">
-          <el-input v-model="form.username" placeholder="请输入用户名" clearable></el-input>
+          <el-input
+            v-model="form.username"
+            placeholder="请输入用户名"
+            clearable
+          ></el-input>
         </el-form-item>
         <el-form-item label="密码">
-          <el-input v-model="form.password" placeholder="请输入密码" show-password clearable></el-input>
+          <el-input
+            v-model="form.password"
+            placeholder="请输入密码"
+            show-password
+            clearable
+          ></el-input>
         </el-form-item>
         <el-form-item>
           <el-button class="el-button--blocked" type="primary">登录</el-button>
