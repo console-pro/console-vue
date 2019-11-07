126b7126fd77722e9b092d760c3616f561a30020
sqrtthree <imsqrtthree@gmail.com>
2019-11-07 23:14:59 +0800

    refactor: Separate build info helper into separate file

--- src/main.js
+++ src/main.js
@@ -8,28 +8,19 @@ import mixins from '@/mixins'
 import store from '@/store'
 import router from '@/router'
 import App from '@/App.vue'
+import outputBuildInfo from '@/helpers/output-build-info'
 
 Vue.use(ElementUI)
 Vue.use(mixins)
 Vue.use(globalRegistration)
-
-// eslint-disable-next-line
-console.log(
-  `%c${BUILD_INFO.name}%cv${BUILD_INFO.version}`,
-  'background: #35495e; color: #fff; padding: 2px 4px; border-radius: 3px 0 0 3px;',
-  'background: #19be6b; color: #fff; padding: 2px 4px; border-radius: 0 3px 3px 0;'
-) // Output build info with colorful support.
-
-// eslint-disable-next-line
-console.log(`Build at ${new Date(BUILD_INFO.time).toLocaleString()}`)
-// eslint-disable-next-line
-console.log(`Hash: ${BUILD_INFO.hash}`)
 
 if (process.env.NODE_ENV === 'development') {
   import('@/helpers/debug').then(debug => {
     debug.default()
   })
 }
+
+outputBuildInfo()
 
 new Vue({
   router,
