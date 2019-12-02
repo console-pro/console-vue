304e36c4a046432f052f92b78c32313c47455e83
sqrtthree <imsqrtthree@gmail.com>
2019-12-02 19:48:25 +0800

    fix: Adjust styles of dropdown menu in headerbar

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,19 @@ export default {
       return this.$store.state.config.theme
     },
   },
+  watch: {
+    '$store.state.config.theme'(value) {
+      this.setThemeToBody(value)
+    },
+  },
+  mounted() {
+    this.setThemeToBody(this.theme)
+  },
+  methods: {
+    setThemeToBody(theme) {
+      document.body.className = `app--${theme}`
+    },
+  },
 }
 </script>
 
