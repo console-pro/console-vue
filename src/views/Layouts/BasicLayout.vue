2a517f8ccb92a9ea504df5b82824da2398d2cf12
sqrtthree <imsqrtthree@gmail.com>
2019-12-09 13:32:22 +0800

    feat: Add page transition animation

--- src/views/Layouts/BasicLayout.vue
+++ src/views/Layouts/BasicLayout.vue
@@ -9,7 +9,15 @@
           <the-header />
         </el-header>
         <el-main>
-          <router-view />
+          <div class="main">
+            <transition
+              name="page-transition-animation"
+              enter-active-class="animated slideInRight"
+              leave-active-class="animated slideOutLeft"
+            >
+              <router-view />
+            </transition>
+          </div>
         </el-main>
         <el-footer>
           <the-footer />
@@ -32,5 +40,148 @@ export default {
   min-height: 100vh;
 
   @include theme();
+}
+
+.main {
+  position: relative;
+  background-color: #fff;
+
+  @include theme-dark {
+    background-color: map-get($theme-dark, 'main-bg');
+  }
+}
+
+.page-transition-animation {
+  &-enter,
+  &-enter-active,
+  &-enter-to,
+  &-leave,
+  &-leave-active,
+  &-leave-to {
+    position: absolute;
+    width: 100%;
+  }
+
+  &-enter,
+  &-enter-active,
+  &-enter-to {
+    z-index: 1;
+  }
+
+  &-leave,
+  &-leave-active,
+  &-leave-to {
+    z-index: 2;
+  }
+}
+
+.animated {
+  -webkit-animation-duration: 1s;
+  animation-duration: 1s;
+  -webkit-animation-fill-mode: both;
+  animation-fill-mode: both;
+}
+
+@-webkit-keyframes zoomIn {
+  0% {
+    opacity: 0;
+    -webkit-transform: scale3d(0.3, 0.3, 0.3);
+    transform: scale3d(0.3, 0.3, 0.3);
+  }
+
+  50% {
+    opacity: 1;
+  }
+}
+
+@keyframes zoomIn {
+  0% {
+    opacity: 0;
+    -webkit-transform: scale3d(0.3, 0.3, 0.3);
+    transform: scale3d(0.3, 0.3, 0.3);
+  }
+
+  100% {
+    opacity: 1;
+  }
+}
+
+.zoomIn {
+  -webkit-animation-name: zoomIn;
+  animation-name: zoomIn;
+}
+
+@-webkit-keyframes slideInRight {
+  0% {
+    -webkit-transform: translate3d(20%, 0, 0);
+    transform: translate3d(20%, 0, 0);
+    visibility: visible;
+    opacity: 0;
+  }
+
+  to {
+    -webkit-transform: translateZ(0);
+    transform: translateZ(0);
+    opacity: 1;
+  }
+}
+
+@keyframes slideInRight {
+  0% {
+    -webkit-transform: translate3d(20%, 0, 0);
+    transform: translate3d(20%, 0, 0);
+    visibility: visible;
+    opacity: 0;
+  }
+
+  to {
+    -webkit-transform: translateZ(0);
+    transform: translateZ(0);
+    opacity: 1;
+  }
+}
+
+.slideInRight {
+  -webkit-animation-name: slideInRight;
+  animation-name: slideInRight;
+}
+
+@-webkit-keyframes slideOutLeft {
+  0% {
+    -webkit-transform: translateZ(0);
+    transform: translateZ(0);
+    opacity: 1;
+  }
+
+  to {
+    visibility: hidden;
+    -webkit-transform: translate3d(-20%, 0, 0);
+    transform: translate3d(-20%, 0, 0);
+    opacity: 0;
+  }
+}
+
+@keyframes slideOutLeft {
+  0% {
+    -webkit-transform: translateZ(0);
+    transform: translateZ(0);
+    opacity: 1;
+  }
+
+  50% {
+    opacity: 0;
+  }
+
+  to {
+    visibility: hidden;
+    -webkit-transform: translate3d(-20%, 0, 0);
+    transform: translate3d(-20%, 0, 0);
+    opacity: 0;
+  }
+}
+
+.slideOutLeft {
+  -webkit-animation-name: slideOutLeft;
+  animation-name: slideOutLeft;
 }
 </style>
