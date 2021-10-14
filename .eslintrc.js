55f9f4f0f542ce13f6f803bfa094bc79a8e457df
sqrtthree <imsqrtthree@gmail.com>
2021-10-14 11:29:19 +0800

    chore: add support for linter

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  extends: ['plugin:import/recommended', 'plugin:prettier/recommended'],
+  rules: {
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+    'import/prefer-default-export': 'off',
+  },
+}
