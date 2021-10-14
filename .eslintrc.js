f0f8e68e886f32544b7a6b3f9e43024e753a988c
sqrtthree <imsqrtthree@gmail.com>
2021-10-14 17:10:45 +0800

    feat: add support for typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,16 @@
 module.exports = {
+  root: true,
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+  },
+  plugins: ['@typescript-eslint'],
   extends: [
     'plugin:import/recommended',
-    'plugin:prettier/recommended',
+    'plugin:import/typescript',
+    'plugin:@typescript-eslint/recommended',
     'plugin:vue/vue3-recommended',
+    'plugin:prettier/recommended',
   ],
   rules: {
     'no-console': ['warn', { allow: ['warn', 'error'] }],
