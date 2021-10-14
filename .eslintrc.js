f19280f2afc6194346c70135dcaf9864657b937f
sqrtthree <imsqrtthree@gmail.com>
2021-10-14 11:31:30 +0800

    chore: enable eslint-plugin-vue

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,9 @@
 module.exports = {
-  extends: ['plugin:import/recommended', 'plugin:prettier/recommended'],
+  extends: [
+    'plugin:import/recommended',
+    'plugin:prettier/recommended',
+    'plugin:vue/vue3-recommended',
+  ],
   rules: {
     'no-console': ['warn', { allow: ['warn', 'error'] }],
     'import/prefer-default-export': 'off',
